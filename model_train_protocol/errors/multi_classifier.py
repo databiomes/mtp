5c812bfda4332e97d1dbaed595e80d7fd49fcf1b
NIKITASZENBOOK\nikit <nikita.olechko@gmail.com>
2026-07-23 19:08:07 -0700

    Add test suite for classification models

---
+++ model_train_protocol/errors/multi_classifier.py
@@ -0,0 +1,7 @@
+"""Errors raised for multi-classifier output validation."""
+
+from .outputs import OutputError
+
+
+class MultiClassifierError(OutputError):
+    """Errors raised for multi-classifier output validation."""
